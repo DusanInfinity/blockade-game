d6cfe5409e02431bb6130c774b8dee7f51567435
stefandobrosavljevic <stefipanter999@gmail.com>
2021-12-11 15:35:40 +0100

    Korigovana funkcija za promenu kordinata Playera, ubacena inicijalna tabla u fajlu Player.py

--- Aplikacija/Blockade/Player.py
+++ Aplikacija/Blockade/Player.py
@@ -1,3 +1,5 @@
+from Node import Node
+
 
 class Player:
     def __init__(self, znak, x, y, board):
@@ -7,24 +9,79 @@ class Player:
         self.board = board
         self.zidovi = 0
 
-    def setPlayer(self, x, y):
+    def postaviIgracaNaTabli(self, x, y, z = None):
+        self.board.board[x * 2][y * 2].setPlayer(x * 2, y * 2, self.znak if z == None else " ")
+
+    def setPlayerCordinates(self, x, y):
+        self.postaviIgracaNaTabli(self.x, self.y, '')
         self.x = x
         self.y = y
+        self.postaviIgracaNaTabli(self.x, self.y)
 
     def movePlayer(self, x, y):
-        if self.validateMoveForDimensions(x, y) and self.validateMoveForWalls(x, y):
-            self.setPlayer(x, y)
+        if self.validateMoveForBoardDimensions(x, y) and self.validateMoveForWalls(x, y):
+            self.setPlayerCordinates(x, y)
         else:
             pass
 
-    def validateMoveForDimensions(self, x, y):
-        if not (x < 0 or x > self.board.X or y < 0 or y > self.board.Y):
+    def validateMoveForBoardDimensions(self, x, y):
+        if x < 0 or x > self.board.width or y < 0 or y > self.board.length:
             return False
         return True
+
+    def validateMoveDirection(self, x, y):
+        pass
 
     def validateMoveForWalls(self, x, y):
         # Da li su zidovi na putanji ka kojoj zelimo da idemo,
         # Ne mozemo da preskocimo zid
+        return True
         pass
 
-    
+
+
+
+class Board:
+    def __init__(self, length, width) -> None:
+        self.length = length
+        self.width = width
+        self.board = []
+        self.players = []
+
+    def postaviTablu(self):
+        for i in range(self.length * 2 - 1):
+            self.board.append([])
+            for j in range(self.width * 2 - 1):
+                if i % 2 == 0:
+                    if j % 2 == 1:
+                        self.board[i].append(Node(i, j, "|"))
+                    else:
+                        self.board[i].append(Node(i, j, " "))
+                else:
+                    if j % 2 == 0:
+                        self.board[i].append(Node(i, j, "-"))
+                    else:
+                        self.board[i].append(Node(i, j, " "))
+
+    def iscrtajTablu(self):
+        for i in range(self.length * 2 - 1):
+            for j in range(self.width * 2 - 1):
+                print(self.board[i][j].char, end="")
+            print("")
+        print("")
+
+    def postaviIgrace(self):
+        self.players.append(Player("X", 3, 3, self))
+        self.players.append(Player("X", 7, 3, self))
+        self.players.append(Player("O", 3, 10, self))
+        self.players.append(Player("O", 7, 10, self))
+        for p in self.players:
+            p.postaviIgracaNaTabli(p.x, p.y)
+
+
+board = Board(11, 14)
+board.postaviTablu()
+board.postaviIgrace()
+board.iscrtajTablu()
+board.players[1].movePlayer(5, 5)
+board.iscrtajTablu()
